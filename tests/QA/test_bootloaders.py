c895cf9fe6b0c8650ee6db6ac92ac8d6a9057adc
Kimberly McGuire <kimberly@bitcraze.io>
2022-07-13 14:05:36 +0200

    remove stress test bootloader temporary

--- tests/QA/test_bootloaders.py
+++ tests/QA/test_bootloaders.py
@@ -34,7 +34,7 @@ class TestBootloaders:
     def test_bootloader_reset_simple(self, dev):
         self.bootloader_back_and_forth(dev)
 
-    def test_bootloader_reset_stress(self, dev):
-        requirement = conftest.get_requirement('bootloaders.reliability')
-        for _ in range(0, requirement['iterations']):
-            self.bootloader_back_and_forth(dev)
+    #def test_bootloader_reset_stress(self, dev):
+    #    requirement = conftest.get_requirement('bootloaders.reliability')
+    #    for _ in range(0, requirement['iterations']):
+    #        self.bootloader_back_and_forth(dev)
