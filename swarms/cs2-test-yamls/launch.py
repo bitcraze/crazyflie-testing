103f8e960cdc2b0e87ae86b1d4a33991475d4ea1
Kimberly McGuire <kimberly@bitcraze.io>
2024-05-08 16:59:34 +0200

    Create launch.py

---
+++ swarms/cs2-test-yamls/launch.py
@@ -0,0 +1,46 @@
+import os
+import yaml
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.conditions import LaunchConfigurationEquals
+from launch.conditions import LaunchConfigurationNotEquals
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        DeclareLaunchArgument('backend', default_value='cpp'),
+        DeclareLaunchArgument('debug', default_value='False'),
+        DeclareLaunchArgument('server_yaml_file', default_value=''),
+        Node(
+            package='crazyflie',
+            executable='crazyflie_server.py',
+            condition=LaunchConfigurationEquals('backend','cflib'),
+            name='crazyflie_server',
+            output='screen',
+            parameters= [PythonExpression(["'tmp_server.yaml' if '", LaunchConfiguration('server_yaml_file'), "' == '' else '", LaunchConfiguration('server_yaml_file'), "'"])],
+        ),
+        Node(
+            package='crazyflie',
+            executable='crazyflie_server',
+            condition=LaunchConfigurationEquals('backend','cpp'),
+            name='crazyflie_server',
+            output='screen',
+            parameters= [PythonExpression(["'tmp_server.yaml' if '", LaunchConfiguration('server_yaml_file'), "' == '' else '", LaunchConfiguration('server_yaml_file'), "'"])],
+            prefix=PythonExpression(['"xterm -e gdb -ex run --args" if ', LaunchConfiguration('debug'), ' else ""']),
+        ),
+        Node(
+            package='crazyflie_sim',
+            executable='crazyflie_server',
+            condition=LaunchConfigurationEquals('backend','sim'),
+            name='crazyflie_server',
+            output='screen',
+            emulate_tty=True,
+            parameters= [PythonExpression(["'tmp_server.yaml' if '", LaunchConfiguration('server_yaml_file'), "' == '' else '", LaunchConfiguration('server_yaml_file'), "'"])],
+        ),
+
+
+    ])
